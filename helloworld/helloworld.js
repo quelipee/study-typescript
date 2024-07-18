2f0645531f41f18a7c544e58971a204404d096c3
felipe <felipemateus112012@gmail.com>
2024-07-18 17:29:12 -0300

    study

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -1,7 +1,12 @@
 "use strict";
 let mensagem = 'hello world'; // definindo a variavel e o tipo dela
-console.log(mensagem);
+// console.log(mensagem);
 function addnumber(numb1, numb2) {
     return numb1 + numb2;
 }
-console.log(addnumber(1, 1));
+// console.log(addnumber(1,1));
+let pessoa;
+pessoa = {
+    nome: 'felipe', idade: 26, altura: 1.50, date: '1997/12/19'
+};
+console.log(pessoa);
--- helloworld/helloworld.ts
+++ helloworld/helloworld.ts
@@ -1,10 +1,22 @@
 let mensagem: string = 'hello world';// definindo a variavel e o tipo dela
 
-console.log(mensagem);
+// console.log(mensagem);
 
 function addnumber(numb1: number, numb2: number ):number
 {
     return numb1 + numb2;
 }
-console.log(addnumber(1,1));
+// console.log(addnumber(1,1));
 
+let pessoa : {
+    nome: string;
+    idade: number;
+    altura: number;
+    date: date;
+}
+
+pessoa = {
+    nome: 'felipe',idade: 26, altura: 1.50, date: '1997/12/19'
+};
+
+console.log(pessoa);
